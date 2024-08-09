db82780e72b218e0e8b838914018d768728717e6
Jim Tang <wxfreakk91@gmail.com>
2024-08-08 19:35:39 -0500

    fetch hybrid sigma data

--- reanalysis_api/datasources/era5.py
+++ reanalysis_api/datasources/era5.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 import pandas as pd
 import xarray as xr
 import pint_xarray  # type: ignore
@@ -22,6 +24,28 @@ def arco_era5_isobaric() -> xr.Dataset:
         chunks=None,
         storage_options=dict(token="anon"),
     )
+
+
+def era5_level_coefficients() -> pd.DataFrame:
+    parent = Path(__name__).parents[0].absolute()
+    datadir = os.path.join(parent, "data")
+    file = os.path.join(datadir, "era5_vertical.txt")
+    cols = [
+        "a",
+        "b",
+        "ph",
+        "pf",
+        "geopotential_altitude",
+        "geometric_altitude",
+        "temperature",
+        "density",
+    ]
+    return pd.read_csv(file, sep="\t", names=cols)
+
+
+def surface_pressure(ds: xr.Dataset) -> float:
+    sfcp = (ds.surface_pressure * units(ds.surface_pressure.units)).pint.to("Pa")
+    return float(sfcp)
 
 
 def transform_era5_isobaric(ds: xr.Dataset) -> Profile:
@@ -96,5 +120,72 @@ def transform_era5_isobaric(ds: xr.Dataset) -> Profile:
     )
 
 
+def transform_era5_native(ds: xr.Dataset, sfc_pres_pa: float) -> Profile:
+    lat = float(ds.coords["latitude"])
+    lon = float(ds.coords["longitude"])
+
+    vert_levels = era5_level_coefficients()
+    pressure = (vert_levels.a + vert_levels.b * sfc_pres_pa)[1:] / 10
+    # hacky here - convert to hPa
+    # pressure /= 100
+
+    height = ds["geopotential"] / mpconst.g
+    temperature = ds["temperature"]
+
+    specific_humidity = ds["specific_humidity"]
+    dewpoint = mpcalc.dewpoint_from_specific_humidity(
+        # conform the dimensions
+        xr.DataArray(pressure, dims=["hybrid"]) * units.hectopascal,
+        temperature * units(temperature.units),
+        specific_humidity * units("kg/kg"),
+    )
+    dewpoint = dewpoint.pint.to(temperature.units)
+
+    u_wind = ds["u_component_of_wind"]
+    v_wind = ds["v_component_of_wind"]
+    omega = ds["vertical_velocity"]
+
+    profile_data = ProfileData(
+        pressure=_reversed(pressure),
+        height=_reversed(height),
+        temperature=_reversed(temperature),
+        dewpoint=_reversed(dewpoint),
+        u_wind=_reversed(u_wind),
+        v_wind=_reversed(v_wind),
+        omega=_reversed(omega),
+    )
+
+    # units
+    pressure_units = "hPa"
+    height_units = "m"
+    temperature_units = temperature.units
+    dewpoint_units = temperature.units
+    u_wind_units = u_wind.units
+    v_wind_units = v_wind.units
+    omega_units = omega.units
+
+    profile_units = ProfileUnits(
+        pressure=pressure_units,
+        height=height_units,
+        temperature=temperature_units,
+        dewpoint=dewpoint_units,
+        u_wind=u_wind_units,
+        v_wind=v_wind_units,
+        omega=omega_units,
+    )
+
+    return Profile(
+        lat=lat,
+        lon=lon,
+        timestamp=pd.Timestamp(ds["time"].values).to_pydatetime(),
+        data=profile_data,
+        units=profile_units,
+    )
+
+
 def _merge(sfc: xr.DataArray | float | None, da: xr.DataArray) -> list[float | None]:
-    return [None if sfc is None else float(sfc)] + list(reversed(da.values.tolist()))
+    return [None if sfc is None else float(sfc)] + _reversed(da)
+
+
+def _reversed(da: xr.DataArray) -> list[float]:
+    return list(reversed(da.values.tolist()))
--- reanalysis_api/main.py
+++ reanalysis_api/main.py
@@ -1,17 +1,32 @@
 from datetime import datetime
 from fastapi import FastAPI, HTTPException
 
-from reanalysis_api.datasources.era5 import transform_era5_isobaric
-from reanalysis_api.lifespan import lifespan, era5_isobaric_pool
+from reanalysis_api.datasources.era5 import (
+    surface_pressure,
+    transform_era5_isobaric,
+    transform_era5_native,
+)
+from reanalysis_api.lifespan import lifespan, era5_isobaric_pool, era5_native_pool
 from reanalysis_api.models import Profile
 
 app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/era5/isobaric")
+@app.get("/era5_isobaric")
 def era5_isobaric_profile(t: datetime, lat: float, lon: float) -> Profile:
     try:
         ds = era5_isobaric_pool.get_at(t, lat, lon)
+        return transform_era5_isobaric(ds)
     except KeyError as e:
         raise HTTPException(status_code=404, detail=str(e))
-    return transform_era5_isobaric(ds)
+
+
+@app.get("/era5_native")
+def era5_native_profile(t: datetime, lat: float, lon: float) -> Profile:
+    try:
+        ds_isobaric = era5_isobaric_pool.get_at(t, lat, lon)
+        ds_native = era5_native_pool.get_at(t, lat, lon)
+        sfc_pressure = surface_pressure(ds_isobaric)
+        return transform_era5_native(ds_native, sfc_pressure)
+    except KeyError as e:
+        raise HTTPException(status_code=404, detail=str(e))
